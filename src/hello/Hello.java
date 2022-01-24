dac4db9606c0641146e3b500ac200d513bfd7599
BoburbekS <91512221+BoburbekS@users.noreply.github.com>
2022-01-24 09:27:17 -0600

    Practice 3

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Practice 1");
         System.out.println("Practice 2");
+        System.out.println("Practice 3");
     }
 }
