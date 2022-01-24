629151f03c0cd05a4c6ca4b622b0193a9ce3c140
BoburbekS <91512221+BoburbekS@users.noreply.github.com>
2022-01-24 09:30:51 -0600

    Test001 3

--- src/hello/TestCase001.java
+++ src/hello/TestCase001.java
@@ -4,5 +4,6 @@ public class TestCase001 {
     public static void main(String[] args) {
         System.out.println("Test001 1");
         System.out.println("Test001 2");
+        System.out.println("Test001 3");
     }
 }
